9ac7b5691535a4eb2d0706014734373b15d39ff7
Tharaka Galappaththi <tharakagalappaththi@Tharakas-MacBook-Pro.local>
2024-07-29 23:30:16 +0530

    ST12 - 5 tcs

---
+++ src/test/java/com/cutanddry/qa/tests/EditProductQtyFrmCatalogTest.java
@@ -0,0 +1,49 @@
+package com.cutanddry.qa.tests;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Customer;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class AddProductsFrmCatalogTest extends TestBase {
+    static User user;
+    static String customerId = "16579";
+    static String itemName = "Artichoke";
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test
+    public static void addProductsFrmCatalog() {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsDistributor(user.getEmailOrMobile(), user.getPassword());
+        Dashboard.isUserNavigatedToDashboard();
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+        Dashboard.navigateToCustomers();
+        Customer.searchCustomerByCode(customerId);
+        softAssert.assertTrue(Customer.isCustomerSearchResultByCodeDisplayed(customerId),"search error");
+        Customer.clickOnOrderGuide(customerId);
+        Customer.goToCatalog();
+        Customer.searchItemOnCatalog(itemName);
+        softAssert.assertTrue(Customer.getFirstElementFrmSearchResults().contains(itemName), "item not found");
+        Customer.addItemToCartCatalog();
+        Customer.checkoutItems();
+        softAssert.assertTrue(Customer.getItemNameFirstRow().contains(itemName),"item mismatch");
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        closeAllBrowsers();
+    }
+
+}
